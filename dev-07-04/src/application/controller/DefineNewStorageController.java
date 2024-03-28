f0834eedb41d82385e3c017d01cebd425d6dac43
PhongDiep <phongdiep0712@gmail.com>
2024-03-28 13:34:19 -0700

    Switched to scenebuilder, created all the required pages, and implemented funcitons for navigation and alert

---
+++ dev-07-04/src/application/controller/DefineNewStorageController.java
@@ -0,0 +1,61 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class DefineNewStorageController {
+	@FXML TextField locationName;
+	@FXML TextArea description;
+	
+	@FXML
+	public void showHomePage(ActionEvent event) {
+		try {
+			// Load the new page FXML file
+	        Parent defineNewCategoryPage = FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
+	        // Get the current stage (window) from the event that triggered the method call
+	        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        // Set the scene with the new page
+	        stage.setScene(new Scene(defineNewCategoryPage));
+	        // Optional: Set the title of the new window (stage)
+	        stage.setTitle("Home");
+	        // Show the new scene
+	        stage.show();
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+	}
+	
+	@FXML
+    private void handleSaveButtonClick() {
+        // Check if the text field is empty
+        if (locationName.getText().trim().isEmpty()) {
+            // Text field is empty, show the alert
+        	// Display error message if the text field is empty
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText(null);
+			alert.setContentText("Please enter a location name.");
+			alert.showAndWait();
+        } else {
+            // Save function goes here....
+			// Display a message to indicate successful saving
+			Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+			successAlert.setTitle("Information");
+			successAlert.setHeaderText(null);
+			successAlert.setContentText("New storage \"" + locationName.getText() + "\" has been created!");
+			successAlert.showAndWait();
+			locationName.clear(); // Clear the text from the TextField
+			description.clear();// Clear the text from the TextArea
+        }
+    }
+
+}
---
+++ dev-07-04/src/application/controller/SampleController.java
@@ -0,0 +1,69 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+public class SampleController {
+	
+	@FXML
+	public void showDefineNewCategoryPage(ActionEvent event) {
+		try {
+			// Load the new page FXML file
+	        Parent defineNewCategoryPage = FXMLLoader.load(getClass().getClassLoader().getResource("view/DefineNewCategory.fxml"));
+	        // Get the current stage (window) from the event that triggered the method call
+	        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        // Set the scene with the new page
+	        stage.setScene(new Scene(defineNewCategoryPage));
+	        // Optional: Set the title of the new window (stage)
+	        stage.setTitle("Define New Category");
+	        // Show the new scene
+	        stage.show();
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+	}
+	
+	@FXML
+	public void showDefineNewStoragePage(ActionEvent event) {
+		try {
+			// Load the new page FXML file
+	        Parent defineNewCategoryPage = FXMLLoader.load(getClass().getClassLoader().getResource("view/DefineNewStorage.fxml"));
+	        // Get the current stage (window) from the event that triggered the method call
+	        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        // Set the scene with the new page
+	        stage.setScene(new Scene(defineNewCategoryPage));
+	        // Optional: Set the title of the new window (stage)
+	        stage.setTitle("Define New Storage");
+	        // Show the new scene
+	        stage.show();
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+	}
+	
+	@FXML
+	public void showHomePage(ActionEvent event) {
+		try {
+			// Load the new page FXML file
+	        Parent defineNewCategoryPage = FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
+	        // Get the current stage (window) from the event that triggered the method call
+	        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        // Set the scene with the new page
+	        stage.setScene(new Scene(defineNewCategoryPage));
+	        // Optional: Set the title of the new window (stage)
+	        stage.setTitle("Home");
+	        // Show the new scene
+	        stage.show();
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+	}
+}
