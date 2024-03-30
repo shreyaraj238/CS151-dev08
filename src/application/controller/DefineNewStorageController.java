217ff45f7212ceec800e4e95dcd447ae2cbeadb6
PhongDiep <phongdiep0712@gmail.com>
2024-03-29 20:09:47 -0700

    Fixed the bug of home button in define new storage page

--- src/application/controller/DefineNewStorageController.java
+++ src/application/controller/DefineNewStorageController.java
@@ -2,6 +2,7 @@ package application.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
